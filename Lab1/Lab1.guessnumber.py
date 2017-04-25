f3144eb5f20277542728c3766925c6347704d5f3
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2017-04-25 13:49:45 +0200

    added extra case: guessed number outside of range 0,20

--- Lab1/Lab1.guessnumber.py
+++ Lab1/Lab1.guessnumber.py
@@ -6,9 +6,12 @@ while guessed==False:
     userNumber = int(input("Introduce your guess number: "))
     if userNumber==randomNumber:
         guessed = True
+	print("Yes, " + str(randomNumber) + " is correct!")
         print("Excellent, Well done!")
+    elif (userNumber > 20) | (userNumber < 0):
+	print("Haha, you're a real joker! The random number is between 0 and 20! Try again.") 
     elif userNumber > randomNumber:
         print("Try one more time, a bit lower")
     elif userNumber < randomNumber:
         print("Try one more time, a bit higher")
-print("Yes, " + str(randomNumber) + " is correct!")
+
